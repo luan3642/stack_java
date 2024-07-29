4cf7f88c336b281e2f913995a3fb9820b7b57afb
luan3642 <luan3642@hotmail.com>
2024-07-29 12:18:16 -0300

    praticando pilha em java

--- stack_java/src/br/com/alldirect/oo/Pilha2.java
+++ stack_java/src/br/com/alldirect/oo/Pilha2.java
@@ -13,6 +13,10 @@ public class Pilha2 {
 	public boolean isEmpty() {
 		return this.topo == -1;
 	}
+	
+	public boolean isFull() {
+		return this.topo == this.pilha.length -1;
+	}
 
 	public void adicionar(int elemento) {
 		if (this.topo == this.pilha.length - 1) {
@@ -20,6 +24,24 @@ public class Pilha2 {
 		}
 
 		this.pilha[++topo] = elemento;
+	}
+	
+	public int peek() {
+		if(this.isEmpty()) {
+			throw new IllegalArgumentException("a pilha está vazia");
+		}
+		return this.pilha[topo];
+	}
+	
+	public int pop() {
+		if(this.isEmpty()) {
+			throw new IllegalArgumentException("a pilha já está vazia");
+		}
+		int elementoRemovido = this.pilha[topo];
+		this.pilha[topo--] = 0;
+		
+		
+		return elementoRemovido;
 	}
 
 	public void exibirPilha() {
@@ -36,10 +58,19 @@ public class Pilha2 {
 	public static void main(String[] args) {
 		Pilha2 pilha2 = new Pilha2(5);
 		
+		pilha2.adicionar(2);
+		pilha2.adicionar(2);
 		pilha2.adicionar(4);
-		pilha2.adicionar(3);
+		pilha2.adicionar(5);
+		
+		
 		
 		System.out.println("a pilha está vazia?"+pilha2.isEmpty());
+		pilha2.exibirPilha();
+		System.out.println();
+		System.out.println("topo da pilha é "+pilha2.peek());
+		System.out.println("a pilha está cheia?"+pilha2.isFull());
+		System.out.println("elemento removido "+pilha2.pop());
 		pilha2.exibirPilha();
 	}
 
